98d3e38ec7b6e331a35f9edcbf4ebe377d396db1
Que Mnqika <qaqambilemnqika23@gmail.com>
2023-05-16 16:04:54 +0200

    js push

--- script/code.js
+++ script/code.js
@@ -1,4 +1,9 @@
 let subject = ["Python", "C++", "VB.Net", "Javascript", "Assembly"]
+let place = subject.length / 2;
 
-console.log(subject[4]);
-console.log(subject[2]);
+//display
+console.log(subject.at(4));
+
+let x = Math.floor(place);
+
+console.log(x % 2 == 0 ? subject[x] : subject.slice(x+1, x-1) );
